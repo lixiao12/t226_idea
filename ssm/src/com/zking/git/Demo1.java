915f4309850148743d4c2541fbc8a32c499fd7c4
lixiao12 <2949424758@qq.com>
2019-11-06 19:29:30 +0800

    本地仓库接受

---
+++ ssm/src/com/zking/git/Demo1.java
@@ -0,0 +1,13 @@
+package com.zking.git;
+
+/**
+ * @author 小李飞刀
+ * @site www.javaxl.com
+ * @company
+ * @create  2019-11-06 19:27
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("idea git demo");
+    }
+}
